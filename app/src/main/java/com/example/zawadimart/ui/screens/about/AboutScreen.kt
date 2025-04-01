8c9a612a2ac9a7a55f3d3881cb48cb365f4fe669
Mutumaaa <mutumasdaily@gmail.com>
2025-04-01 22:08:05 +0300

    Initial commit

---
+++ app/src/main/java/com/example/zawadimart/ui/screens/about/AboutScreen.kt
@@ -0,0 +1,18 @@
+package com.example.zawadimart.ui.screens.about
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun AboutScreen(){
+
+
+}
+
+@Preview
+@Composable
+fun AboutScreenPreview(){
+
+
+
+}
